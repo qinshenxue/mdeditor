f9f066f182f3dd5598170d1f2dfeb209f3cb5306
笔心 <qinshenxue@163.com@c44cecf0-df93-4034-aa40-e3797c37f998>
2016-02-26 07:26:58 +0000

    no commit message

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp'),
+    rename = require("gulp-rename"),
+    uglify = require('gulp-uglify');
+
+gulp.task('compress', function () {
+    return gulp.src('mdeditor.js')
+        .pipe(gulp.dest('./'))
+        .pipe(uglify())
+        .pipe(rename({extname: ".min.js"}))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('default', ['compress']);
