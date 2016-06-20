58985484624465b028fcf9144124549587b79cc0
笔心 <qinshenxue@163.com>
2016-06-20 02:13:53 +0000

    增加node-ftp同步到mdeditor.qinshenxue.com

---
+++ sync.js
@@ -0,0 +1,21 @@
+
+
+var Client = require('ftp');
+var syncConfig=require('./sync-config');
+
+var c = new Client();
+c.on('ready', function() {
+    console.log('start sync');
+    c.put('dist/mdeditor.min.css','/demo/mdeditor/css/mdeditor.min.css',function(err){
+        if(err) throw err;
+        c.end();
+        console.log('css sync success');
+    })
+    c.put('dist/mdeditor.min.js','/demo/mdeditor/js/mdeditor.min.js',function(err){
+        if(err) throw err;
+        c.end();
+        console.log('js sync success');
+    })
+
+});
+c.connect(syncConfig);
