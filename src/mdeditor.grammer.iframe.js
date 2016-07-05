15661447c95602d9570a4c5bcfe6806c174a9b96
笔心 <qinshenxue@163.com@c44cecf0-df93-4034-aa40-e3797c37f998>
2016-07-05 01:38:31 +0000

    修复不设置高度，style无效的问题

--- src/mdeditor.grammer.iframe.js
+++ src/mdeditor.grammer.iframe.js
@@ -3,10 +3,11 @@ mdeditor && mdeditor.addGrammar && mdeditor.addGrammar({
     handle: function (rows, i, that) {
         var row = rows[i];
         row = row.replace(that.reg, function (match, $1, $2) {
-            var style = "";
+            var style = 'style="display:block;border:0;width:100%;';
             if ($1 != '' && !isNaN($1)) {
-                style = 'style="display:block;border:0;width:100%;height:' + $1 + 'px"';
+                style += 'height:' + $1 + 'px"';
             }
+            style += '"';
             return '<iframe class="mdeditor-iframe" src="' + $2 + '" ' + style + '></iframe>';
         });
         return {
