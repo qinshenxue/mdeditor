a8406efd351327f845f288da9c06026ffd7740e1
笔心 <qinshenxue@163.com>
2016-07-06 06:06:11 +0000

    no commit message

--- sync.js
+++ sync.js
@@ -1,28 +1,28 @@
 // 将最新文件同步到项目主页中
 var Client = require('ftp');
 // ftp的配置信息，包括host，user，password
-var syncConfig=require('./sync-config');
+var syncConfig = require('./sync-config');
 
 var c = new Client();
-c.on('ready', function() {
+c.on('ready', function () {
     console.log('start sync');
-    c.put('dist/mdeditor.min.css','/demo/mdeditor/css/mdeditor.min.css',function(err){
-        if(err) throw err;
+    c.put('dist/mdeditor.min.css', '/demo/mdeditor/css/mdeditor.min.css', function (err) {
+        if (err) throw err;
         c.end();
         console.log('mdeditor.min.css sync success');
     })
-    c.put('dist/mdeditor.min.js','/demo/mdeditor/js/mdeditor.min.js',function(err){
-        if(err) throw err;
+    c.put('dist/mdeditor.min.js', '/demo/mdeditor/js/mdeditor.min.js', function (err) {
+        if (err) throw err;
         c.end();
         console.log('mdeditor.min.js sync success');
     });
-    c.put('dist/mdeditor.grammer.iframe.min.js','/demo/mdeditor/js/mdeditor.grammer.iframe.min.js',function(err){
-        if(err) throw err;
+    c.put('dist/mdeditor.grammer.iframe.min.js', '/demo/mdeditor/js/mdeditor.grammer.iframe.min.js', function (err) {
+        if (err) throw err;
         c.end();
         console.log('mdeditor.grammer.iframe.min.js sync success');
     })
-    c.put('demo/index.html','/demo/mdeditor/index.html',function(err){
-        if(err) throw err;
+    c.put('demo/index.html', '/demo/mdeditor/index.html', function (err) {
+        if (err) throw err;
         c.end();
         console.log('index.html sync success');
     })
