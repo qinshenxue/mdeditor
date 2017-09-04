eb64c32110d39d364259d27f87075abcbe894ace
qinshenxue <qinshenxue@qq.com>
2017-09-04 08:08:02 +0800

    title 解析

--- src/markdown.js
+++ src/markdown.js
@@ -190,7 +190,7 @@ function toTree(rows: Array<string>) {
             html.push({
                 tag: 'h' + hno[0].length,
                 md: row,
-                html: handleInlineSet(row.replace(hFlagReg, ''))
+                html: handleInlineSet(row.replace(hFlagReg, '').slice(1))  // slice(1) 去除空格
             })
 
         } else if (regLib.hr.test(row)) {
